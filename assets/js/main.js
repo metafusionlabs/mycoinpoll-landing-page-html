8c1898eac2671fb31d0e9bfb807ddb954b496f7c
juelthpi <juelcse2019@gmail.com>
2025-01-24 23:01:17 +0600

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+jQuery(document).ready(function ($) {
+    $(document).on("click", function (event) {
+      const $div = $("#outside-click");
+      const $navbarText = $("#navbarText");
+      if (!$div.is(event.target) && $div.has(event.target).length === 0) {
+        $navbarText.removeClass("show");
+      }
+    });
+  // menu bg add
+    $(window).on('scroll', function() {
+      if ($(window).scrollTop() > 300) {
+          $('header').addClass('menu-bg-fixed');
+      } else {
+          $('header').removeClass('menu-bg-fixed');
+      }
+  });
+})
